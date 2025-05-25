32342ce1efd0631088e467404f48ba557644b3d0
Tooonia <tooonia@gmail.com>
2025-05-25 20:24:44 +0200

    Removing unnecessary comment from main method in PixelartBackApplication.

--- src/main/java/com/simplon/pixelartback/PixelartBackApplication.java
+++ src/main/java/com/simplon/pixelartback/PixelartBackApplication.java
@@ -15,35 +15,6 @@ public class PixelartBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PixelartBackApplication.class, args);
 	}
-//	@Bean
-//	public PixelArtMapper pixelArtMapper() {
-//		return new PixelArtMapper() {
-//			@Override
-//			public PixelArtEntity dtoToEntity(PixelArtDto dto) {
-//				return null;
-//			}
-//
-//			@Override
-//			public void dtoToEntity(PixelArtDto dto, PixelArtEntity entity) {
-//
-//			}
-//
-//			@Override
-//			public List<PixelArtEntity> dtosToEntities(List<PixelArtDto> dtos) {
-//				return null;
-//			}
-//
-//			@Override
-//			public PixelArtDto entityToDto(PixelArtEntity entity) {
-//				return null;
-//			}
-//
-//			@Override
-//			public List<PixelArtDto> entitiesToDtos(Collection<PixelArtEntity> entities) {
-//				return null;
-//			}
-//		};
-//	}
 
 
 }
